d328abf078d0915f8b6718f8ac083b6da02cce1d
Vishal Gupta <vk3946111@gmail.com>
2024-03-20 19:40:04 +0530

    Updates

---
+++ precache-manifest.dc0868c3a03d05f0397c8e68dd90c2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18e112da8863c7911d6268c941fd124d",
+    "url": "/Text-Manipulation/index.html"
+  },
+  {
+    "revision": "6ba5cf61e726c28323a7",
+    "url": "/Text-Manipulation/static/css/main.e270d4b8.chunk.css"
+  },
+  {
+    "revision": "2fc9793c1ff35f777b37",
+    "url": "/Text-Manipulation/static/js/2.1b5a1e9d.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/Text-Manipulation/static/js/2.1b5a1e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65cd859f106b0a944654",
+    "url": "/Text-Manipulation/static/js/3.3963f254.chunk.js"
+  },
+  {
+    "revision": "6ba5cf61e726c28323a7",
+    "url": "/Text-Manipulation/static/js/main.a36b0176.chunk.js"
+  },
+  {
+    "revision": "e9ebcfd1a1c7b1c36bca",
+    "url": "/Text-Manipulation/static/js/runtime-main.b8446e80.js"
+  }
+]);
